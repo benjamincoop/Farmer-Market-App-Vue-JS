cb872e53357c1f6744e05cb9b7793de12ae5d5d1
Weston Chan <westonchan.888@hotmail.com>
2020-08-27 20:00:55 -0500

    Split into seperate files

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+	cat_produce: 'Produce',
+	cat_baked: 'Baked Goods',
+	cat_meat: 'Meat, Poultry, and Fish',		
+	produce_item_1: 'Fruits',
+	produce_item_2: 'Leafy Vegetables',
+	produce_item_3: 'Tubers and Non-Leafy Vegetables',
+	baked_item_1: 'Cakes',
+	baked_item_2: 'Cookies',
+	baked_item_3: 'Pies',
+	meat_item_1: 'Meat',
+	meat_item_2: 'Poultry',
+	meat_item_3: 'Fish'
+  },
+  methods: {
+	toggleCollapse(e) {
+	  var collapsibleContent = e.target.nextElementSibling;
+	  if (collapsibleContent.style.maxHeight){
+	  collapsibleContent.style.maxHeight = null;
+	  } else {
+		collapsibleContent.style.maxHeight = collapsibleContent.scrollHeight + 'px';
+	  } 
+	}
+  }
+})

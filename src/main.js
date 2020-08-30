720de925089d3df71661aa41b0b754657814f7c7
Weston Chan <westonchan.888@hotmail.com>
2020-08-30 14:20:43 -0500

    Single File Components working

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import FarmersMarketApp from './FarmersMarketApp.vue';
+
+new Vue({
+  el: '#app',
+  render: h => h(FarmersMarketApp),
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// Source Here: https://www.freecodecamp.org/news/how-to-create-a-vue-js-app-using-single-file-components-without-the-cli-7e73e5b8244f/
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+    ]
+  },
+  devServer: {
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+};

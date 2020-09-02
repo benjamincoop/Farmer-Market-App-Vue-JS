2713c0a8e8a22fa0f104378d718caf9d0b337cb3
Benjamin Cooper <benjaminc@ksu.edu>
2020-09-01 19:10:19 -0500

    added basic routing

--- src/FarmersMarketApp.vue
+++ src/FarmersMarketApp.vue
@@ -1,8 +1,14 @@
 <template>
     <div id="app">
-        <sliding-menu></sliding-menu>
+      <sliding-menu></sliding-menu>
+
+      <template v-if="CurrentPage == 'home'">
         <h3>Product Categories</h3>
         <collapsible-category :categoryName="'Root'"></collapsible-category>
+      </template>
+      <template v-else-if="CurrentPage == 'contacts'">
+        <h3>Contacts</h3>
+      </template>
     </div>
 </template>
 
@@ -10,6 +16,9 @@
     import CollapsibleCategory from "./CollapsibleCategory.vue";
     import SlidingMenu from "./SlidingMenu.vue";
     export default {
+        data(){ return {
+          CurrentPage: "home"
+        }},
         components: {
             CollapsibleCategory,
             SlidingMenu,

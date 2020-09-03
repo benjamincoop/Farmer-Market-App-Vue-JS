9af1fc554b528712a3a8372ce0c3f811822edcb6
Weston Chan <westonchan.888@hotmail.com>
2020-09-02 22:39:21 -0500

    Finish entry and add offline message

--- src/CollapsibleCategory.vue
+++ src/CollapsibleCategory.vue
@@ -1,32 +1,34 @@
 <template>
-    <div>
+    <div class="container">
         <button class="collapsible" v-on:click.self="open = !open">{{ categories[categoryName].name }}</button>
         
-        <div v-if="open" class="content">
-            <ul v-if="categories[categoryName].children.length !== 0">
-                <template v-for="entry in categories[categoryName].children">
-                    <collapsible-category
-                        v-if="categories[entry].type === 'category'"
-                        :key="categories[entry].name"
-                        :categoryName="categories[entry].name"
-                    ></collapsible-category>
-                    
-                    <li
-                        v-else-if="categories[entry].type === 'entry'"
-                        :key="categories[entry].name"
-                    >
-                        {{ categories[entry].name }}
-                    </li>
-                </template>
-            </ul>
+        <div v-if="categories[categoryName].children.length !== 0 && open" class="content">
+            <template v-for="entry in categories[categoryName].children">
+                <collapsible-category
+                    v-if="categories[entry].type === 'category'"
+                    :key="categories[entry].name"
+                    :categoryName="categories[entry].name"
+                ></collapsible-category>
+                <button
+                    v-else-if="categories[entry].type === 'entry'"
+                    class="entry"
+                    :key="categories[entry].name"
+                    @click.self="onEntryClick(categories[entry].name)">
+                    {{ categories[categoryName].name }}
+                </button>
+            </template>
         </div>
     </div>
 </template>
 
 <script>
-import {mapGetters} from 'Vuex';
+import {mapGetters, mapMutations} from 'Vuex';
+import CategoryEntry from './CategoryEntry.vue';
 export default {
     name: "collapsible-category",
+    components: {
+        CategoryEntry,
+    },
     data() {
         return {
             open: false,
@@ -39,10 +41,54 @@ export default {
         },
     },
     computed: {
-        ...mapGetters(['categories']),
+        ...mapGetters(['categories', 'currentEntry']),
+    },
+    methods: {
+        ...mapMutations(['setCurrentEntry']),
+        onEntryClick(entryName) {
+            if (this.categories[entryName].type === 'entry') {
+                this.setCurrentEntry(entryName);
+            }
+        },
     },
 }
 </script>
 
 <style>
+.collapsible {
+    background-color: #002569;
+    color: white;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+    border: 1px solid #F1AD02;
+    margin: 5px 0;
+  }
+
+.entry {
+  background-color: #e2c16c;
+  color: white;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+  border: 1px solid #F1AD02;
+  margin: 5px 0;
+}
+  
+.active, .collapsible:hover {
+  border-width: 3px;
+  background-color: #365faa
+}
+
+.content {
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+  background-color: #f1f1f1;
+}
 </style>
---
+++ src/ProductListPage.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h3>Product Categories</h3>
+        <div class='category-container'>
+            <collapsible-category
+                v-for="entry in categories['Root'].children"
+                class = 'inline-block'
+                :key="categories[entry].name"
+                :categoryName="categories[entry].name"
+            ></collapsible-category>
+            <category-entry
+                class='inline-block container'
+            ></category-entry>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'Vuex';
+import CollapsibleCategory from "./CollapsibleCategory.vue";
+import CategoryEntry from "./CategoryEntry.vue";
+export default {
+    name: 'ProductListPage',
+    components: {
+        CollapsibleCategory,
+        CategoryEntry,
+    },
+    computed: {
+        ...mapGetters(['categories']),
+    },
+}
+</script>
+
+<style scoped>
+
+
+
+
+@media screen and (min-width: 600px) {
+    .category-container {
+        display: flex;
+    }
+    .inline-block {
+        flex: 50%;
+    }
+}
+
+@media screen and (max-width: 600px) {
+    #collapsible-category-entry {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+    }
+}
+</style>

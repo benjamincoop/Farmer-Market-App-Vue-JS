e5aca08589bed7bb1430c46ae4dd652ec83d748b
Weston Chan <westonchan.888@hotmail.com>
2020-09-04 11:00:45 -0500

    new build

--- src/main.js
+++ src/main.js
@@ -23,6 +23,6 @@ new Vue({
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js');
+    navigator.serviceWorker.register('service-worker.js', { scope: "/" });
   });
 }

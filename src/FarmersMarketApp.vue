95b4d4f26621d66d39c68989fc6819580cb36fc8
Michael Brinkman <mbrinkman1026@gmail.com>
2020-09-08 22:51:10 -0500

    add comments to my js features

--- src/FarmersMarketApp.vue
+++ src/FarmersMarketApp.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app" class="full-height">
     <div v-if="!online" class="alert">You are currently offline!</div>
+
+    <!-- A hidden banner that shows and hides based on a styling change defined below as the js function "alert" and "dismiss" -->
     <div id="alert">Alert!<button v-on:click="dismiss" class="dismiss-btn">X</button></div>
+
+    <!-- The menu viewable when the screen width is greater than 600px -->
     <desktop-menu class='desktop-only'></desktop-menu>
+
+    <!-- The menu viewable when the screen width is less than 600px -->
     <sliding-menu class='mobile-only'></sliding-menu>
+
     <template v-if="CurrentPage == 'home'">
       <product-list-page></product-list-page>
     </template>
@@ -16,6 +23,8 @@
     <template v-else-if="CurrentPage == 'reporting'">
       <reporting-page></reporting-page>
     </template>
+
+    <!-- Triggers alert function defined below -->
     <button style="position: absolute; bottom: 10px;" v-on:click="alert">Test Alert</button>
   </div>
 </template>
@@ -44,11 +53,11 @@
         },
         methods: {
           alert: function (event) {
-            // `this` inside methods points to the Vue instance
+            //Lowers the alert banner to the top of the screen
             document.getElementById("alert").style.top = 0;
-            // `event` is the native DOM event
           },
           dismiss: function (event) {
+            //Raises the alert banner back above the top of the screen
             document.getElementById("alert").style.top = "-40px";
           }
         }
